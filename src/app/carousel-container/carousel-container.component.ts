644356f0557bd07ac33882eb4698ef26c484311d
Shawn Jackson <shawn.anthony.j.95@gmail.com>
2025-01-08 17:52:23 -0800

    cycle items around sides, fix direction

--- src/app/carousel-container/carousel-container.component.ts
+++ src/app/carousel-container/carousel-container.component.ts
@@ -30,16 +30,72 @@ export class CarouselContainerComponent {
     this.activeIndex =
       this.activeIndex === 0 ? this.paths.length - 1 : this.activeIndex - 1;
 
-    let images = document.getElementById('repeater')?.children;
-    if (!images) return;
-    for (let i = 0; i < 5; i++) {
-      console.log(images[i]);
+    const repeater = document.getElementById('repeater');
+    let images = repeater?.children;
+    if (!repeater || !images) return;
+    // (images[0] as HTMLMediaElement).classList.add('left-img');
+    (images[0] as HTMLMediaElement).classList.remove('ghost-img');
+
+    // let curr_translation = parseFloat(
+    //   (repeater.style.transform.match(/\((.*)%/) || [0, '0'])[1]
+    // );
+    // console.log(curr_translation);
+    // repeater.style.transform = `translateX(${
+    //   curr_translation + 33.333333333
+    // }%)`;
+
+    for (let i = 0; i < images.length; i++) {
+      let image = images[i] as HTMLElement;
+      let curr_translation = parseInt(
+        (image.style.transform.match(/\((.*)%/) || [0, '0'])[1]
+      );
+      image.style.transform = `translateX(${curr_translation + 100}%)`;
     }
+
+    // (images[0] as HTMLMediaElement).classList.remove('left-img');
+
+    (images[3] as HTMLMediaElement).classList.add('ghost-img');
+
+    let last = images[4] as HTMLMediaElement;
+    last.src =
+      this.paths[
+        (this.paths.length + this.activeIndex - 2) % this.paths.length
+      ];
+    // last.classList.add('left-img');
+    repeater.prepend(last);
   };
 
   slideRight = () => {
     this.activeIndex =
       this.activeIndex === this.paths.length - 1 ? 0 : this.activeIndex + 1;
+
+    const repeater = document.getElementById('repeater');
+    let images = repeater?.children;
+    if (!repeater || !images) return;
+
+    (images[4] as HTMLMediaElement).classList.remove('ghost-img');
+
+    // let curr_translation = parseFloat(
+    //   (repeater.style.transform.match(/\((.*)%/) || [0, '0'])[1]
+    // );
+    // console.log(curr_translation);
+    // repeater.style.transform = `translateX(${
+    //   curr_translation - 33.333333333
+    // }%)`;
+
+    // for (let i = 0; i < images.length; i++) {
+    //   let image = images[i] as HTMLElement;
+    //   let curr_translation = parseInt(
+    //     (image.style.transform.match(/\((.*)%/) || [0, '0'])[1]
+    //   );
+    //   image.style.transform = `translateX(${curr_translation - 100}%)`;
+    // }
+
+    (images[1] as HTMLMediaElement).classList.add('ghost-img');
+
+    (images[0] as HTMLMediaElement).src =
+      this.paths[(this.activeIndex + 2) % this.paths.length];
+    repeater.append(images[0]);
   };
 
   slideTo = (index: number) => {
